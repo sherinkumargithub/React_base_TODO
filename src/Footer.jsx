b4417c2d5a06230d102211ce2d9c54a2b148d94e
sherinkumargithub <sherindev404@gmail.com>
2024-11-03 13:26:56 +0530

    third commit

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,15 +1,29 @@
+// import React from 'react'
+
+
+// // reciving the props
+// const Footer = ({length}) => {
+//     const year = new Date();
+//   return (
+//     // <footer>Copyright &copy; {year.getFullYear()}</footer>
+//     <footer>
+//       {length} list of {length === 1 ? "activity" : "activities"}
+//     </footer>
+//   )
+// }
+
+// export default Footer  
 import React from 'react'
 
-
-// reciving the props
-const Footer = ({length}) => {
-    const year = new Date();
-  return (
-    // <footer>Copyright &copy; {year.getFullYear()}</footer>
-    <footer>
-      {length} list of {length === 1 ? "activity" : "activities"}
-    </footer>
-  )
+// receiving the props
+const Footer = ({ length }) => {
+    const year = new Date().getFullYear();
+    return (
+        <footer>
+            <p>Copyright &copy; {year}</p>
+            <p>{length} {length === 1 ? "activity" : "activities"} in the list</p>
+        </footer>
+    )
 }
 
-export default Footer  
+export default Footer;
